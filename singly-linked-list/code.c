fa75bacf3e0e3f5837202774482bea62fa142bcc
Arvid Danielsson <arvda096@student.liu.se>
2023-01-27 15:59:01 +0100

    lab0 done

--- singly-linked-list/code.c
+++ singly-linked-list/code.c
@@ -11,42 +11,37 @@ struct list_item{
 void append(struct list_item * first, int x){ 
 /* puts x at the end
 of the list */
-printf("yo");
     struct list_item* last = (struct list_item*) malloc(sizeof(struct list_item));
     last->value = x;
     last->next = NULL;
-  //  if(first != NULL){
-        while(first->next != NULL){
-            first = first->next;            
+    struct list_item* cur;
+    struct list_item* prev;
+    cur = first;
+        while(cur->next != NULL){
+        cur = cur->next;
         }
-        first->next = last;
-   // }
+    cur->next = last;
 }
+
 
 
 void prepend(struct list_item * first , int x){
  /* puts x at the
 beginning of the list */
     struct list_item* newfirst = (struct list_item*) malloc(sizeof(struct list_item));
-    newfirst->next = first;
+    struct list_item* temp = first->next;
+    first->next = newfirst;
+    newfirst->next = temp;
     newfirst->value = x;
-
- //   struct list_item *next = first->next;
-   // while(cur != NULL){
-     //   cur->next = cur;
-       // cur = next;
-    //}
-//    first->value = x;
 }
 
 
 void print(struct list_item * first){
 /* prints all elements in
 the list */
-    struct list_item *cur = first;
-    printf("HEJ");
+    struct list_item *cur = first->next;
     while(cur != NULL){
-        printf("%d", cur->value);
+        printf("%d \n", cur->value);
         cur = cur->next;
     }
 }
@@ -60,15 +55,16 @@ and input x right before that element */
     newElem->value = x;
     struct list_item*  prev = first;
 
-    while(cur!=NULL){
+    while(cur->next !=NULL){
         if(cur->value < x)
         {
-            prev = cur;
+            prev = cur; 
             cur = cur->next;
         }
         else{
             newElem->next = cur;
             prev->next = newElem;
+            break;
         }
     }
 }
@@ -77,20 +73,29 @@ and input x right before that element */
 void clear(struct list_item *first){
  /* free everything dynamically allocated */
     struct list_item* next;
-    while(first != NULL){
+    while(first->next != NULL){
         next = first->next;
-        free(first);
-        first = next;
+        first->next = next->next;
+        free(next);
     }
 }
 
 
 int main( int argc, char ** argv) {
-    printf("HELLO");
 struct list_item root;
 root.value = -1; /*This value is always ignored */
 root.next =NULL;
-    append(root.next, 5);
-    print(root.next);
-    clear(root.next);
+    append(&root, 1);
+    append(&root, 2);
+    append(&root, 3);
+    append(&root, 4);
+    append(&root, 6);
+    append(&root, 7);
+    prepend(&root, -10);
+    input_sorted(&root,5);
+    print(&root);
+    clear(&root);
+    append(&root, 100);
+    print(&root);
+    print(&root);
 }
